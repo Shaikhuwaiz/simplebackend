2169df3d49c995d73d0fdf0c45eed4295a4ae924
CodeSandbox <support@codesandbox.io>
2025-07-02 13:06:02 +0000

    Sandbox snapshot commit

--- index.ts
+++ index.ts
@@ -2,10 +2,17 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello CodeSandbox!");
+// Serve static files from 'public'
+app.use(express.static("public"));
+
+app.get("/submit", (req, res) => {
+  const name = req.query.name;
+  const age = req.query.age;
+  console.log("Received name:", name);
+  console.log("Received age:", age);
+  res.send(`Received, ${name}! and you are ${age} Old`);
 });
 
 app.listen(port, () => {
-  console.log(`Sandbox listening on port ${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
