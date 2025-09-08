68a28e2f727545a74caf963258ce6d4e1ee57bfa
Owaiz Shaikh <your_github_email@example.com>
2025-09-08 15:18:49 +0530

    fix cors to *

--- index.ts
+++ index.ts
@@ -15,13 +15,7 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin:"*",
     methods: ["GET", "POST", "OPTIONS"],
     credentials: true,
   })
